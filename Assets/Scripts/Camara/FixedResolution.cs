e89b7973243444276648c96edb407b2cddbff1b4
Gerardo Yerena Celestin <yerenac.gera@gmail.com>
2026-06-07 17:05:16 -0600

    Arreglo de Resolucion de camara

---
+++ Assets/Scripts/Camara/FixedResolution.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FixedResolution : MonoBehaviour
+{
+    [Tooltip("Aspecto objetivo")]
+    public float aspectoObjetivo = 16f / 9f;
+
+    private Camera cam;
+    private int anchoAnterior;
+    private int altoAnterior;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+        AplicarAspecto();
+    }
+
+    void Update()
+    {
+        if (Screen.width != anchoAnterior || Screen.height != altoAnterior)
+            AplicarAspecto();
+    }
+
+    void AplicarAspecto()
+    {
+        anchoAnterior = Screen.width;
+        altoAnterior = Screen.height;
+
+        float aspectoActual = (float)Screen.width / Screen.height;
+        float escala = aspectoActual / aspectoObjetivo;
+
+        Rect rect = new Rect(0, 0, 1, 1);
+
+        if (escala < 1f)
+        {
+            // Pantalla más alta de lo esperado → barras arriba/abajo (letterbox)
+            rect.height = escala;
+            rect.y = (1f - escala) / 2f;
+        }
+        else
+        {
+            // Pantalla más ancha de lo esperado → barras izq/der (pillarbox)
+            rect.width = 1f / escala;
+            rect.x = (1f - rect.width) / 2f;
+        }
+
+        cam.rect = rect;
+    }
+}
