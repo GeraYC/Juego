4ff773d18f3292be1332018c19be40cde56476fc
Mare-moto <mare.vega08@gmail.com>
2026-06-08 23:08:28 -0600

    cht

--- Assets/Scripts/Player/Movimiento del Jugador.cs
+++ Assets/Scripts/Player/Movimiento del Jugador.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour //pito pal yerestin
 {
-    public float velocidad = 5f;
+    public float velocidad = 5f; //materia de mierda
 
     private Rigidbody rb;
     private Animator animator;
